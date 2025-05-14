fc8250eab41384cf8d1d20a68367a0ae58041c1b
ipz233_lda <ipz233_lda@student.ztu.edu.ua>
2025-05-14 17:42:06 +0300

    Додано патерн State

---
+++ Lab03/Task06/EditState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    public class EditState : IEditorState
+    {
+        public void Render()
+        {
+            Console.WriteLine("Rendering in Edit Mode");
+        }
+    }
+}
