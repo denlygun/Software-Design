c3724bc592d5c5455d0940db9f32e20ce8485366
ipz233_lda <ipz233_lda@student.ztu.edu.ua>
2025-05-14 17:44:17 +0300

    Додано ValidationVisitor

---
+++ Lab03/Task06/ValidationVisitor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    public class ValidationVisitor : IVisitor
+    {
+        public void VisitDiv(DivElement div)
+        {
+            Console.WriteLine("Validating Div...");
+        }
+
+        public void VisitSpan(SpanElement span)
+        {
+            Console.WriteLine("Validating Span...");
+        }
+    }
+}
