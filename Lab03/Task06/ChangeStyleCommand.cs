c333a49cb1fda063c241bdfe784f27cbe9b4e7d9
ipz233_lda <ipz233_lda@student.ztu.edu.ua>
2025-05-14 17:38:38 +0300

    Додано патерн Command

---
+++ Lab03/Task06/ChangeStyleCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    public class ChangeStyleCommand
+    {
+        private HtmlElement element;
+        private string newStyle;
+        private string oldStyle;
+
+        public ChangeStyleCommand(HtmlElement element, string newStyle)
+        {
+            this.element = element;
+            this.newStyle = newStyle;
+            this.oldStyle = element.Style;
+        }
+
+        public void Execute()
+        {
+            element.Style = newStyle;
+        }
+
+        public void Undo()
+        {
+            element.Style = oldStyle;
+        }
+    }
+}
