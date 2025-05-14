e40716d6462a2ebb7edc0036a6741e0301b32a32
ipz233_lda <ipz233_lda@student.ztu.edu.ua>
2025-05-14 17:43:13 +0300

    Додано базові інтерфейси Visitor

---
+++ Lab03/Task06/IVisitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    public interface IVisitor
+    {
+        void VisitDiv(DivElement div);
+        void VisitSpan(SpanElement span);
+    }
+}
