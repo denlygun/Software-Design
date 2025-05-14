17d6e2a082590f91779f694e1b41f8cb4ad7aa56
ipz233_lda <ipz233_lda@student.ztu.edu.ua>
2025-05-14 17:32:48 +0300

    Додано ітератор в глибину

---
+++ Lab03/Task06/DepthIterator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    public class DepthIterator
+    {
+        private Stack<HtmlElement> stack;
+
+        public DepthIterator(HtmlElement root)
+        {
+            stack = new Stack<HtmlElement>();
+            stack.Push(root);
+        }
+
+        public bool HasNext()
+        {
+            return stack.Count > 0;
+        }
+
+        public HtmlElement Next()
+        {
+            HtmlElement current = stack.Pop();
+            for (int i = current.Children.Count - 1; i >= 0; i--)
+            {
+                stack.Push(current.Children[i]);
+            }
+            return current;
+        }
+    }
+
+}
